c04c647aa94290123cc4db1ec7dfd650a1431f51
Owen Mundy (from server) <omundy@gmail.com>
2019-12-12 08:45:50 -0800

    Adds mobile button to trail status

--- sections/section-trail-status.php
+++ sections/section-trail-status.php
@@ -1,62 +1,3 @@
-
-
-
-
-<!-- 
-<div class="container-fluid trail-status">
-
-
-<i class="fa fa-mobile" aria-hidden="true"></i>
-
-<div class="container">
-<div class="row align-items-center">
-<div class="col-lg-4">
-<ul>
-    <li class="unavailable"><span class="trail-name">Anne Springs</span> <span class="time-info">7/16-7:17 am</span></li>
-    <li class="available"><span class="trail-name">Backyard Trails</span> <span class="time-info">8/23-10:57 am</span></li>
-    <li class="unavailable"><span class="trail-name">Ballantyne District Park</span> <span class="time-info">8/23-10:57 am</span></li>
-    <li class="questionable"><span class="trail-name">Big Leaf Slopes Park</span> <span class="time-info">8/23-10:58 am</span></li>
-    <li class="available"><span class="trail-name">Cedar Valley Bike Park</span> <span class="time-info">8/29-5:31 am</span></li>
-    <li class="available"><span class="trail-name">Col. Francis Beatty Park</span> <span class="time-info">8/23-10:58 am</span></li>
-    <li class="available"><span class="trail-name">Fisher Farm Park</span> <span class="time-info">8/16-7:17 am</span></li>
-    <li class="available"><span class="trail-name">Harrisburg Half</span> <span class="time-info">8/23-10:59 am</span></li>
-</ul>
-</div>
-
-<div class="col-lg-4">
-<ul>
-    <li class="available"><span class="trail-name">Huntersville Elem. School </span> <span class="time-info">7/16-7:17 am</span></li>
-    <li class="available"><span class="trail-name">Jetton Park</span> <span class="time-info">8/23-10:57 am</span></li>
-    <li class="available"><span class="trail-name">Lake Norman State Park</span> <span class="time-info">8/23-10:57 am</span></li>
-    <li class="available"><span class="trail-name">Mazeppa Park</span> <span class="time-info">8/23-10:58 am</span></li>
-    <li class="questionable"><span class="trail-name">Mountain Island Park</span> <span class="time-info">8/29-5:31 am</span></li>
-    <li class="unavailable"><span class="trail-name">North Mecklenburg Park</span> <span class="time-info">8/23-10:58 am</span></li>
-    <li class="available"><span class="trail-name">Park Road Park</span> <span class="time-info">8/16-7:17 am</span></li>
-    <li class="unavailable"><span class="trail-name">Purser Hulsey Park</span> <span class="time-info">8/23-10:59 am</span></li>
-</ul>
-</div>
-
-<div class="col-lg-4">
-<ul>
-    <li class="unavailable"><span class="trail-name">Renaissance Park</span> <span class="time-info">7/16-7:17 am</span></li>
-    <li class="available"><span class="trail-name">Rocky Branch Trail</span> <span class="time-info">8/23-10:57 am</span></li>
-    <li class="unavailable"><span class="trail-name">Rocky River Trail</span> <span class="time-info">8/23-10:57 am</span></li>
-    <li class="questionable"><span class="trail-name">Sherman Branch</span> <span class="time-info">8/23-10:58 am</span></li>
-    <li class="available"><span class="trail-name">Signal Hill</span> <span class="time-info">8/29-5:31 am</span></li>
-    <li class="available"><span class="trail-name">Southwest District Park</span> <span class="time-info">8/23-10:58 am</span></li>
-    <li class="available"><span class="trail-name">USNWC</span> <span class="time-info">8/16-7:17 am</span></li>
-    <li class="available"><span class="trail-name">Uwharrie</span> <span class="time-info">8/23-10:59 am</span></li>
-</ul>
-</div>
-</div>
-
-</div>
-</div> -->
-
-
-
-
-
 <?php
 
 // will do this later ...
@@ -76,6 +17,7 @@ if (count($trails)){
 
     // start section 
     print '<div class="container-fluid trail-status pb-4">';
+    print '<div class="trail-status-mobile-btn"><a href="/trail-status-json/"><i class="fa fa-mobile" aria-hidden="true"></i></a></div>';
     print '<div class="container">';
     print '<div class="row align-items-center">';
 
@@ -144,3 +86,57 @@ if (count($trails)){
 
 
 
+
+
+
+
+
+<!-- 
+<div class="container-fluid trail-status">
+
+
+
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-4">
+<ul>
+    <li class="unavailable"><span class="trail-name">Anne Springs</span> <span class="time-info">7/16-7:17 am</span></li>
+    <li class="available"><span class="trail-name">Backyard Trails</span> <span class="time-info">8/23-10:57 am</span></li>
+    <li class="unavailable"><span class="trail-name">Ballantyne District Park</span> <span class="time-info">8/23-10:57 am</span></li>
+    <li class="questionable"><span class="trail-name">Big Leaf Slopes Park</span> <span class="time-info">8/23-10:58 am</span></li>
+    <li class="available"><span class="trail-name">Cedar Valley Bike Park</span> <span class="time-info">8/29-5:31 am</span></li>
+    <li class="available"><span class="trail-name">Col. Francis Beatty Park</span> <span class="time-info">8/23-10:58 am</span></li>
+    <li class="available"><span class="trail-name">Fisher Farm Park</span> <span class="time-info">8/16-7:17 am</span></li>
+    <li class="available"><span class="trail-name">Harrisburg Half</span> <span class="time-info">8/23-10:59 am</span></li>
+</ul>
+</div>
+
+<div class="col-lg-4">
+<ul>
+    <li class="available"><span class="trail-name">Huntersville Elem. School </span> <span class="time-info">7/16-7:17 am</span></li>
+    <li class="available"><span class="trail-name">Jetton Park</span> <span class="time-info">8/23-10:57 am</span></li>
+    <li class="available"><span class="trail-name">Lake Norman State Park</span> <span class="time-info">8/23-10:57 am</span></li>
+    <li class="available"><span class="trail-name">Mazeppa Park</span> <span class="time-info">8/23-10:58 am</span></li>
+    <li class="questionable"><span class="trail-name">Mountain Island Park</span> <span class="time-info">8/29-5:31 am</span></li>
+    <li class="unavailable"><span class="trail-name">North Mecklenburg Park</span> <span class="time-info">8/23-10:58 am</span></li>
+    <li class="available"><span class="trail-name">Park Road Park</span> <span class="time-info">8/16-7:17 am</span></li>
+    <li class="unavailable"><span class="trail-name">Purser Hulsey Park</span> <span class="time-info">8/23-10:59 am</span></li>
+</ul>
+</div>
+
+<div class="col-lg-4">
+<ul>
+    <li class="unavailable"><span class="trail-name">Renaissance Park</span> <span class="time-info">7/16-7:17 am</span></li>
+    <li class="available"><span class="trail-name">Rocky Branch Trail</span> <span class="time-info">8/23-10:57 am</span></li>
+    <li class="unavailable"><span class="trail-name">Rocky River Trail</span> <span class="time-info">8/23-10:57 am</span></li>
+    <li class="questionable"><span class="trail-name">Sherman Branch</span> <span class="time-info">8/23-10:58 am</span></li>
+    <li class="available"><span class="trail-name">Signal Hill</span> <span class="time-info">8/29-5:31 am</span></li>
+    <li class="available"><span class="trail-name">Southwest District Park</span> <span class="time-info">8/23-10:58 am</span></li>
+    <li class="available"><span class="trail-name">USNWC</span> <span class="time-info">8/16-7:17 am</span></li>
+    <li class="available"><span class="trail-name">Uwharrie</span> <span class="time-info">8/23-10:59 am</span></li>
+</ul>
+</div>
+</div>
+
+</div>
+</div> -->
